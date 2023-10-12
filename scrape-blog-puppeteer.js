a965517b28cc2f5dea6cac258c38808a41738e6e
Rafał <justregularusersrsly@gmail.com>
2023-10-12 15:07:12 +0200

    saving content

--- scrape-blog-puppeteer.js
+++ scrape-blog-puppeteer.js
@@ -1,19 +1,46 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
 
-  // Navigate to the blog page
-  await page.goto('https://www.timedoctor.com/blog/how-to-start-a-call-center/');
+  const articleUrls = [
+    'https://www.timedoctor.com/blog/how-to-get-call-center-contracts/',
+    'https://www.timedoctor.com/blog/responsible-outsourcing/',
+    'https://www.timedoctor.com/blog/call-center-workforce-management/',
+    'https://www.timedoctor.com/blog/call-center-workforce-management/',
 
-  // Wait for the target element to be available
-  await page.waitForSelector('#penci-post-entry-inner');
+  ];
 
-  // Extract the content
-  const content = await page.$eval('#penci-post-entry-inner', (element) => element.textContent);
+  for (let i = 0; i < articleUrls.length; i++) {
+    const articlePage = await browser.newPage();
+    const articleUrl = articleUrls[i];
 
-  console.log('Article Content:', content);
+    await articlePage.goto(articleUrl);
+
+    // Wait for the target element to be available
+    await articlePage.waitForSelector('#penci-post-entry-inner');
+
+    // Extract the content
+    const content = await articlePage.$eval('#penci-post-entry-inner', (element) => element.textContent);
+
+    const dataToSave = {
+      content: content,
+    };
+
+    const filename = `article_${i + 1}.json`;
+    const jsonData = JSON.stringify(dataToSave, null, 2);
+
+    fs.writeFileSync(filename, jsonData, 'utf-8');
+
+    console.log(`Content from ${articleUrl} saved to ${filename}`);
+
+    // Add a delay between requests (e.g., 5 seconds)
+    await articlePage.waitForTimeout(5000);
+
+    await articlePage.close();
+  }
 
   await browser.close();
 })();
+
